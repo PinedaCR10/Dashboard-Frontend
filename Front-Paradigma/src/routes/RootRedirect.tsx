5d676bc3f11edc47117632d8ac92ed65d1e5a2e3
ItsChavesCR <achaba03x@gmail.com>
2024-11-13 20:59:11 -0600

    login

--- Front-Paradigma/src/routes/RootRedirect.tsx
+++ Front-Paradigma/src/routes/RootRedirect.tsx
@@ -1,7 +1,14 @@
+import { useAuthContext } from './AuthContext'; // Usamos el contexto para verificar autenticación
+import { Navigate } from 'react-router-dom';
 
+const RootRedirect = () => {
+  const { isAuthenticated } = useAuthContext();
 
-export const RootRedirect = () => {
-  return (
-    <div>RootRedirect</div>
-  )
-}
+  return isAuthenticated ? (
+    <Navigate to="/home" replace />
+  ) : (
+    <Navigate to="/login" replace />
+  );
+};
+
+export default RootRedirect;
