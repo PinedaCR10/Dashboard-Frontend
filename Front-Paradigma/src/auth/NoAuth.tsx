93558d8e3ba54f79b1bc94153b8089e2a89fa6ed
Alexa Genar <gomezgenesisalexa03@gmail.com>
2024-11-15 19:30:13 -0600

    feat: ProtectedRoute

---
+++ Front-Paradigma/src/auth/NoAuth.tsx
@@ -0,0 +1,19 @@
+
+
+const NoAuth = () => {
+    return (
+      <div>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+          <div className="w-[700px] h-full pt-10 flex ">
+            <img
+              src="https://www.bluehost.com/blog/wp-content/smush-webp/2023/06/what-is-a-401-error-1024x576.png.webp"
+              alt="Error 401 - No autorizado"
+              className="w-auto h-auto object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default NoAuth;
--- Front-Paradigma/src/routes.tsx
+++ Front-Paradigma/src/routes.tsx
@@ -1,25 +1,28 @@
-import { createBrowserRouter, } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import { Charts } from './pages/Charts';
-import { Dashboards } from './pages/Dashboard';    
+import { Dashboards } from './pages/Dashboard';
+import Protected from './components/Protected/Protected';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: (
-        <Home />
-    ),
+    element: <Home />,
   },
   {
     path: '/charts',
     element: (
-        <Charts /> 
+      <Protected>
+        <Charts />
+      </Protected>
     ),
   },
   {
     path: '/dashboards',
     element: (
-        <Dashboards /> 
+      <Protected>
+        <Dashboards />
+      </Protected>
     ),
   },
 ]);
