1cb0b027e6035ea5d5fb907ecdca3424c90bdb77
ItsChavesCR <achaba03x@gmail.com>
2024-11-16 00:33:11 -0600

    Fix: Test with SignalR connection

--- Front-Paradigma/src/pages/index.tsx
+++ Front-Paradigma/src/pages/index.tsx
@@ -1,5 +1,5 @@
 
 export { default as WelcomeBanner } from "../components/WelcomeBanner/WelcomeBanner";
 export { NavigationPanel } from "../components/NativationPanel/NativationPanel";    
-export { default as SummaryCards } from "../components/SummaryCards/SummaryCards";
+
 export { RecentActivity } from "../components/RecentActivity/RecentActivity";    
