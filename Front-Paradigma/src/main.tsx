34c1018e4f7bc52e42b820c3cb24a4765145aa93
Alexa Genar <gomezgenesisalexa03@gmail.com>
2024-11-17 22:57:08 -0600

    fix; protected route

--- Front-Paradigma/src/main.tsx
+++ Front-Paradigma/src/main.tsx
@@ -2,18 +2,21 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import UserProvider from './context/UserProvider/UserProvider';
-import { router } from './routes'; 
+import UserProvider from './context/UserProvider/UserProvider';// Importa el AuthProvider creado
+import { router } from './routes';
 import './index.css';
+import { AuthProvider } from './routes/AuthContext';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <RouterProvider router={router} />
-      </UserProvider>
+      <AuthProvider> {/* Envolvemos el árbol con AuthProvider */}
+        <UserProvider>
+          <RouterProvider router={router} />
+        </UserProvider>
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
 );
