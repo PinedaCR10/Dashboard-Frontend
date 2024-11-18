705396ddb59a0d72b96733cc99f45aa31a95bc07
Alexa Genar <gomezgenesisalexa03@gmail.com>
2024-11-17 23:28:33 -0600

    fix

--- Front-Paradigma/src/components/Protected/Protected.tsx
+++ Front-Paradigma/src/components/Protected/Protected.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../routes/AuthContext";
+
 
 const Protected = ({ children }: { children: React.ReactNode }) => {
   const { isLogged } = useAuth();
